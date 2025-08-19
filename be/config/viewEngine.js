fe8cebc44ffbca1eb53b41f666c322d88c75c81b
pmhieu27 <26a4041237@hvnh.edu.vn>
2025-08-19 19:06:55 +0700

    update model

--- be/config/viewEngine.js
+++ be/config/viewEngine.js
@@ -2,11 +2,11 @@ const path = require("path");
 const express = require("express");
 
 const configViewEngine = (app) => {
-  app.set("views", path.join("./src", "views"));
+  app.set("views", path.join("./be", "views"));
   app.set("view engine", "ejs");
 
   //static files
-  app.use(express.static(path.join("./src", "public")));
+  app.use(express.static(path.join("./be", "public")));
 };
 
 module.exports = configViewEngine;
--- be/controllers/homeController.js
+++ be/controllers/homeController.js
@@ -12,25 +12,43 @@ const getLoginPage = (req, res) => {
   return res.send("<h1>Welcome to the Login Page</h1>");
 };
 
-const postCreateUser = (req, res) => {
+const getCreatePage = (req, res) => {
+  return res.render("create.ejs", { title: "Create User Page" });
+};
+
+const postCreateUser = async (req, res) => {
   console.log(req.body);
   //   let email = req.body.email;
   //   let name = req.body.name;
   //   let city = req.body.city;
   let { email, name, city } = req.body;
   console.log(email, name, city);
-  connection.query(
+  // connection.query(
+  //   "INSERT INTO users (email, name, city) VALUES (?, ?, ?)",
+  //   [email, name, city],
+  //   function (error, results, fields) {
+  //     if (error) {
+  //       console.error("Error inserting user:", error);
+  //       return res.status(500).send("Error creating user");
+  //     }
+  //     console.log("User created successfully:");
+  //   }
+  // );
+  // connection.query("select * from users", function (error, results, fields) {
+  //   console.log(results);
+  // });
+
+  let [results, fields] = await connection.query(
     "INSERT INTO users (email, name, city) VALUES (?, ?, ?)",
-    [email, name, city],
-    function (error, results, fields) {
-      if (error) {
-        console.error("Error inserting user:", error);
-        return res.status(500).send("Error creating user");
-      }
-      console.log("User created successfully:");
-    }
+    [email, name, city]
   );
-  res.send("User created successfully!");
+
+  console.log("User created successfully:", results);
+
+  res.send("User created successfully");
+
+  // const [results, fields] = await connection.query("select * from users");
+  // console.log(results);
 };
 
 module.exports = {
@@ -38,4 +56,5 @@ module.exports = {
   getRegisterPage,
   getLoginPage,
   postCreateUser,
+  getCreatePage,
 };
