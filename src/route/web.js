270d1f1b49a8dd9773b1aa2a37053baabe99cd5a
CallingSun <87486718+CallingSun@users.noreply.github.com>
2025-08-23 20:23:13 +0700

    Delete src directory

--- src/route/web.js
+++
@@ -1,8 +0,0 @@
-const express = require('express'); // Import the express module
-const { getHomepage, Locc, cmm } = require('../controller/Homecontroller'); // Import the controller functions
-const router = express.Router(); // Create a new router instance
-
-router.get('/', getHomepage); // Route to the getHomepage function in the controller
-router.get('/aaa', Locc); // Route to the Locc function in the controller
-router.get('/abc', cmm); // Route to the cmm function in the controller
-module.exports = router;
